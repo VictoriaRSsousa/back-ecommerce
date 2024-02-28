5bb64f8c8a29595e12328ce81223155c6e83a41a
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-02-28 13:17:26 -0300

    att

--- src/models/user.js
+++ src/models/user.js
@@ -1,39 +1,36 @@
-const connection =  require('../databases/e-commerceConnections')
+const connection = require("../databases/e-commerceConnections");
 
-
-async function list(){
-    const result = await connection.query(`select * from users;`)
-    return result.rows
+async function list() {
+  const result = await connection.query(`select * from users;`);
+  return result.rows;
 }
 
-async function create(user){
-    const transaction = await connection.connect()
-    try{
-       await transaction.query('begin;')
- 
-       const {name,email,password} = user
-       const query = `insert into users (name,email,password) values($1,$2,$3) returning user_id;`
-       const params =[name,email,password]
-       const result = await transaction.query(query,params)
-       const idNewUser = result.rows[0].user_id
- 
-       await transaction.query('commit;')
-       return {
-          id:idNewUser,
-          ...user
-       }
- 
-    }catch{
-       await transaction.query('rollback;')
-       
- 
-    }finally {
-    transaction.release()}
+async function create(user) {
+  const transaction = await connection.connect();
+  try {
+    await transaction.query("begin;");
+
+    const { name, email, password } = user;
+    const query = `insert into users (name,email,password) values($1,$2,$3) returning user_id;`;
+    const params = [name, email, password];
+    const result = await transaction.query(query, params);
+    const idNewUser = result.rows[0].user_id;
+
+    await transaction.query("commit;");
+    return {
+      id: idNewUser,
+      ...user,
+    };
+  } catch {
+    await transaction.query("rollback;");
+  } finally {
+    transaction.release();
+  }
 }
-async function findByEmail(email){
-   const query = `SELECT email,password from pessoas where email = $1`
-   const result = await connection.query(query,[email])
-   return result.rows
+async function findByEmail(email) {
+  const query = `SELECT email,password from pessoas where email = $1`;
+  const result = await connection.query(query, [email]);
+  return result.rows;
 }
 
-module.exports = {list,create}
+module.exports = { list, create };
