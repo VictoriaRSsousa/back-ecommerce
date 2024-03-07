34beb8308c2f5a28181bd2c8190c0c6e7ab0f0d7
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-03-07 15:40:39 -0300

    att

--- src/app.js
+++ src/app.js
@@ -12,12 +12,6 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-//FALTA BOTA A CATEGORIA NOME
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlY29tbWVyY2UgYmFjayIsImF1ZCI6ImVjb21tZXJjZSBmcm9udCIsImlhdCI6MTcwOTQ5MzA1MSwiZXhwIjoxNzEwMDk3ODUxfQ.uhz7bAG4vp5VwtNB8Y1ID-zoHv7uuaquWpF02__PJ4g
-
-//PORTA QUE A APLICAÇÃO VAI RODAR
-
 const port = process.env.PORT_SERVER || 3000
 const app = express()
 
@@ -26,7 +20,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/products',productRoute)
-app.use('/users',userRoute)  // mudar nome para cadastro
+app.use('/users',userRoute)  
 app.use('/sales',saleRoute)
 app.use('/saleDates',saleDateRoute)
 app.use('/login',loginRoute)
